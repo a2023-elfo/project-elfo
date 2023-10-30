ac1b6f52fd1853678560bf9b12aa92a4b11b1afe
MathieuMercier <merciermathieu412@gmail.com>
2023-10-30 11:42:35 -0400

    Timer retout a 90 deg

--- parcours-elfo/src/brasCapteur/BrasCapteur.cpp
+++ parcours-elfo/src/brasCapteur/BrasCapteur.cpp
@@ -1,17 +1,26 @@
 #include <brasCapteur/BrasCapteur.h>
 
 
-void BrasEtCapteur::setupCapteurBras(uint8_t moteur, uint8_t pin, uint8_t angle ){
+void BrasEtCapteur::setupBrasEtCapteur(uint8_t moteur, uint8_t pin, uint8_t angle, int distance ){
     this->moteur = moteur;
     this->angle = angle;
     this->inputPin = pin;
+    this->distanceVoulu = distance;
     SERVO_Enable(moteur);
+    SOFT_TIMER_Enable();
 }
 
-void BrasEtCapteur::loopCapteurBras(){
+void BrasEtCapteur::loopBrasEtCapteur(){
     int distance = ROBUS_ReadIR(inputPin);
-    if(distance < 600){
+    if(distance < distanceVoulu){
         SERVO_SetAngle(moteur, angle);
     }
+
+    if(brasActione){
+        SOFT_TIMER_SetCallback(1, moteurRetour);
+    }
+}
+
+void moteurRetour(){
 
 }
--- parcours-elfo/src/brasCapteur/BrasCapteur.h
+++ parcours-elfo/src/brasCapteur/BrasCapteur.h
@@ -6,7 +6,9 @@ class BrasEtCapteur{
         int inputPin = 0;
         int moteur = 0;
         int angle = 90;
+        int distanceVoulu =0;
+        bool brasActione = false;
     public:
-        void setupCapteurBras(uint8_t,uint8_t,uint8_t);
-        void loopCapteurBras();
+        void setupBrasEtCapteur(uint8_t,uint8_t,uint8_t,int);
+        void loopBrasEtCapteur();
 };
