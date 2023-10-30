fc976479055de2002b2afa2144ed530a4050968a
Mathieu Mercier <merciermathieu412@gmail.com>
2023-10-30 08:51:23 -0400

    ajout customization

--- parcours-elfo/src/brasCapteur/BrasCapteur.cpp
+++ parcours-elfo/src/brasCapteur/BrasCapteur.cpp
@@ -1,15 +1,15 @@
 #include <brasCapteur/BrasCapteur.h>
 
 
-void BrasEtCapteur::setupCapteurBras(){
-    pinMode(inputPin, INPUT);
-
+void BrasEtCapteur::setupCapteurBras(uint8_t moteur, uint8_t pin, uint8_t angle ){
+    this->moteur = moteur;
+    this->angle = angle;
+    this->inputPin = pin;
     SERVO_Enable(moteur);
 }
 
 void BrasEtCapteur::loopCapteurBras(){
-    int distance = analogRead(inputPin);
-
+    int distance = ROBUS_ReadIR(inputPin);
     if(distance < 600){
         SERVO_SetAngle(moteur, angle);
     }
--- parcours-elfo/src/brasCapteur/BrasCapteur.h
+++ parcours-elfo/src/brasCapteur/BrasCapteur.h
@@ -3,11 +3,10 @@
 
 class BrasEtCapteur{
     private:
-        int inputPin;
-        int motorOutputPin;
-        int moteurMouvement;
+        int inputPin = 0;
         int moteur = 0;
+        int angle = 90;
     public:
-        void setupCapteurBras();
+        void setupCapteurBras(uint8_t,uint8_t,uint8_t);
         void loopCapteurBras();
 };
