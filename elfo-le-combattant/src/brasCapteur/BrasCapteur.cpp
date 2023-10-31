b565819244719d775b00bcef39fd2b827f1fd7cb
Jean-Nicolas Gosselin <jeannicolasgosselin@gmail.com>
2023-10-31 16:11:58 -0400

    bras+pince
    ajustement

--- elfo-le-combattant/src/brasCapteur/BrasCapteur.cpp
+++ elfo-le-combattant/src/brasCapteur/BrasCapteur.cpp
@@ -6,18 +6,18 @@ void BrasEtCapteur::setupBrasEtCapteur(uint8_t pin){
 void BrasEtCapteur::batonSortieGauche(){
     SERVO_Enable(this->inputPin);
     SERVO_SetAngle(this->inputPin,0);
-    delay(100);
+    delay(500);
     SERVO_Disable(this->inputPin);
 }
 void BrasEtCapteur::batonSortieDroit(){
     SERVO_Enable(this->inputPin);
     SERVO_SetAngle(this->inputPin,170);
-    delay(100);
+    delay(500);
     SERVO_Disable(this->inputPin);
 }
 void BrasEtCapteur::batonRange(){
     SERVO_Enable(this->inputPin);
     SERVO_SetAngle(this->inputPin,90);
-    delay(100);
+    delay(500);
     SERVO_Disable(this->inputPin);
 }
