59117a6da0005bf6b69300b3ea1156771f5ce740
Zacharie08 <cameron.zacharie@gmail.com>
2023-11-02 10:10:33 -0400

    je push hardcode

--- elfo-le-combattant/src/main.cpp
+++ elfo-le-combattant/src/main.cpp
@@ -13,6 +13,7 @@ Inclure les librairies de functions que vous voulez utiliser
 #include <Arduino.h>
 #include <math.h>
 #include <LibRobus.h> // Essentielle pour utiliser RobUS
+#include <capteurCouleur/capteurCouleur.h>
 
 /* ****************************************************************************
 Variables globales et defines
@@ -305,14 +306,278 @@ if(ROBUS_ReadIR(3) < 100){
     if (ROBUS_ReadIR(3) > 400){
         shortcut();
         if (ROBUS_ReadIR(3) < 25 && ROBUS_ReadIR(0) < 25)
-            {/*hard coder le reste du virage ici apres avoir sorti du shortcut*/
+            { MOTOR_SetSpeed(LEFT, 0.6);
+            MOTOR_SetSpeed(RIGHT, 0.5);
             }
     }
 
 }
 }
-void loop() {
 
+void virage90(){
+
+MOTOR_SetSpeed(LEFT,0.2);
+MOTOR_SetSpeed(RIGHT,-0.2);
+delay(1180);
+arret();
+}
+
+void virage45(){
+MOTOR_SetSpeed(LEFT,0.2);
+MOTOR_SetSpeed(RIGHT,-0.2);
+delay(590);
+arret();
+
+}
+
+void avance_vert(){
+MOTOR_SetSpeed(LEFT, 0.3);
+MOTOR_SetSpeed(RIGHT, 0.305);
+delay(7300);
+
+
+
+}
+void avance_jaune(){
+MOTOR_SetSpeed(LEFT, 0.3);
+MOTOR_SetSpeed(RIGHT, 0.305);
+delay(10220);
+
+}
+
+void avance_loop(){
+
+    MOTOR_SetSpeed(LEFT, 0.3);
+    MOTOR_SetSpeed(RIGHT, 0.305);
+    
+}
+
+void avance8(){
+MOTOR_SetSpeed(LEFT, 0.3);
+MOTOR_SetSpeed(RIGHT, 0.305);
+delay(11700);
+
+}
+
+void virage_vert_complet(){
+avance_loop();
+delay(3000);
+arret();
+delay(500);
+virage90();
+avance_vert();
+virage90();
+avance_loop();
+delay(3000);
+avance8();
+
+}
+
+virage_jaune_complet(){
+
+avance_loop();
+delay(3000);
+arret();
+delay(500);
+virage90();
+avance_jaune();
+virage90();
+avance_loop();
+delay(3000);
+avance8();
+virage45();
+}
+
+
+void virage_vert_complet_2(){
+avance_loop();
+delay(3000);
+arret();
+delay(500);
+virage90();
+avance_vert();
+virage90();
+avance_loop();
+delay(3000);
 shortcut_complet_jump();
+
+}
+
+
+
+
+void CapteurCouleur::setupCapteurCouleur() {
+    tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_700MS, TCS34725_GAIN_1X);
+    
+    if (tcs.begin()) {
+        Serial.println("Found sensor");
+    } else {
+        Serial.println("Cry, no color for you");
+    }
+}
+
+uint8_t CapteurCouleur::lireCouleur() {
+    uint16_t r, g, b, c, colorTemp, lux;
+    
+    tcs.getRawData(&r, &g, &b, &c);
+    colorTemp = tcs.calculateColorTemperature(r, g, b);
+
+    // RED was around 44800
+    // YELLOW was around 2000
+    // GREEN was around 4500
+    // BLUE was around 2600+
+    Serial.print("Color Temp: "); Serial.print(colorTemp, DEC); Serial.println(" K");
+
+    // Return the most prominent color as a uint8_t
+    // use color temp in decimal base
+    if (colorTemp > 0 && colorTemp < 2150) {
+        return JAUNE;
+    } else if (colorTemp > 2150 && colorTemp < 3200) {
+        return BLANC;
+    } else if (colorTemp > 3200 && colorTemp < 6000) {
+        return VERT;
+    } else if (colorTemp > 6000 && colorTemp < 20000) {
+        return VERT;
+    } else if (colorTemp > 20000) {
+        return ROUGE;
+    } else {
+        return INCONNU;
+    }
+}
+
+String CapteurCouleur::couleurToString(uint8_t couleur) {
+    switch (couleur) {
+        case ROUGE:
+            return "Rouge";
+            break;
+        case VERT:
+            return "Vert";
+            break;
+        case BLEU:
+            return "Bleu";
+            break;
+        case JAUNE:
+            return "Jaune";
+            break;
+        case NOIR:
+            return "Noir";
+            break;
+        case BLANC:
+            return "Blanc";
+            break;
+        default:
+            return "Inconnu";
+            break;
+    }
+}
+
+case 3: // On est arrivé pour faire tomber le verre
+        startColor == 'v';
+            moteur.avancerLigneDroite(0.2);
+            // Dépendant de notre couleur, on check un des deux capteurs de distance d'APP1
+            if (startColor == colorSensor.VERT && digitalRead(VERTE_GAUCHE) == LOW) {
+                baton.batonSortieGauche();
+                // On attends que le capteur est aligné avec l'arrière
+                while(digitalRead(ROUGE_GAUCHE) == HIGH) {
+                    delay(0);
+                }
+                MOTOR_SetSpeed(LEFT, 0);
+                MOTOR_SetSpeed(RIGHT, 0);
+                moteur.moteurArret();
+                delay(1000);
+                MOTOR_SetSpeed(LEFT, 0.2);
+                delay(1000);
+                MOTOR_SetSpeed(LEFT, -0.2);
+                delay(1000);
+                MOTOR_SetSpeed(LEFT, 0);
+                baton.batonRange();
+                moteur.avancerLigneDroite(0.2);
+                valEtat++;
+            } 
+            else if (startColor == colorSensor.BLEU && digitalRead(ROUGE_DROITE) == LOW) {
+                baton.batonSortieDroit();
+                // On attends que le capteur est aligné avec l'arrière
+                while(digitalRead(VERTE_GAUCHE) == HIGH) {
+                    delay(0);
+                }
+                moteur.moteurArret();
+                delay(1000);
+                MOTOR_SetSpeed(RIGHT, 0.2);
+                delay(1000);
+                MOTOR_SetSpeed(RIGHT, -0.2);
+                delay(1000);
+                MOTOR_SetSpeed(RIGHT, 0);
+                baton.batonRange();
+                moteur.avancerLigneDroite(0.2);
+                valEtat++;
+            }
+            break;
+        case 4: // On se rend au suiveur de ligne
+            pince.pinceOuvert();
+            delay(1000);
+            while (ROBUS_ReadIR(0) < 588)
+            {
+                ligne.suivreLigne();
+            }
+            MOTOR_SetSpeed(LEFT, 0);
+            MOTOR_SetSpeed(RIGHT, 0);
+            delay(1000);
+            pince.pinceFerme();
+            delay(500);
+            MOTOR_SetSpeed(LEFT, -0.2);
+            MOTOR_SetSpeed(RIGHT, -0.2);
+            delay(1500);
+            MOTOR_SetSpeed(LEFT, 0);
+            MOTOR_SetSpeed(RIGHT, 0);
+            MOTOR_SetSpeed(LEFT, 0.1);
+            delay(2500);
+            MOTOR_SetSpeed(LEFT, 0.2);
+            MOTOR_SetSpeed(RIGHT, 0.2);
+            delay(2000);
+            ligne.suivreLigne();
+ 
+            if (colorSensor.JAUNE == colorSensor.getCouleur()) {
+                valEtat++;
+            }
+
+
+
+
+
+void loop(){
+
+//shortcut_complet_jump();
+avance();
+
+
+
+if (ROBUS_ReadIR(3) < 30 && capteurcouleur.lireCouleur() == vert){
+
+    virage_vert_complet();
+
+}
+
+if (ROBUS_ReadIR(3) < 30 && capteurcouleur.lireCouleur() == jaune){
+
+    virage_jaune_complet();
+
+}
+
+
+
+suivre_ligne();
+
+virage45();
+virage90();
+avance();
+delay(200;)
+virage90();
+virage90();
+virage90();
+avance8();
+
+virage_vert_complet_2();
+
+
 
 }
